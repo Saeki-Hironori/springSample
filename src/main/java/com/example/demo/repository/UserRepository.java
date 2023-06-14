bdc0bfaaf060f170fcba9478e4cd431523484ba7
Saeki Hironori <saeki.hironori@itfllc.co.jp>
2023-06-14 11:49:38 +0900

    initial push

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.User;
+
+/**
+ * ユーザー情報 Repository
+ */
+public interface UserRepository extends JpaRepository<User, Long> {}
